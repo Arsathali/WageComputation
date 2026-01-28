58ab90348ed60034e83da7102a8c7ace29a1f1b2
arsath ali <arsatharsha17@gmail.com>
2026-01-28 14:24:55 +0530

    commited calculate wage

--- src/com/employee/service/EmployeeWageServiceImpl.java
+++ src/com/employee/service/EmployeeWageServiceImpl.java
@@ -1,13 +1,14 @@
 package com.employee.service;
-
 import java.util.ArrayList;
 import java.util.Random;
+
+import com.employee.model.Employee;
 
 public class EmployeeWageServiceImpl implements EmployeeWageServices {
     
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_TIME_HOURS = 8;
-    public static final int PART_TIME_HOURS = 8;
+    public static final int PART_TIME_HOURS = 6;
     public static final int MAX_WORKING_DAYS = 20;
     public static final int MAX_WORKING_HOURS = 100;
 
@@ -16,13 +17,53 @@ public class EmployeeWageServiceImpl implements EmployeeWageServices {
     private ArrayList<Integer> dailyWages = new ArrayList<>();
 
 
-    private int  getAttendamce(){
+    private int  getAttendance(){
         return  random.nextInt(2);
     }
 
-    @Override
-    public void calculateWage(){
+    private int getWorkingHours() {
+        int empType = random.nextInt(3);
+        switch (empType) {
+            case 1: return FULL_TIME_HOURS;
+            case 2: return PART_TIME_HOURS;
+            default: return 0;
+        }
+    }
 
+    @Override
+    public void calculateWage(Employee employee){
+
+        int totalDays = 0;
+        int totalHours = 0;
+        int totalWage = 0;
+
+        dailyWages.clear();
+
+        while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
+            totalDays++;
+
+            int attendance = getAttendance();
+            int workHours = 0;
+
+            if (attendance == 1) {
+                workHours = getWorkingHours();
+            }
+
+            if (totalHours + workHours > MAX_WORKING_HOURS) {
+                workHours = MAX_WORKING_HOURS - totalHours;
+            }
+
+            int dailyWage = workHours * WAGE_PER_HOUR;
+
+            totalHours += workHours;
+            totalWage += dailyWage;
+
+            dailyWages.add(dailyWage);
+        }
+
+        employee.setTotalWorkingDays(totalDays);
+        employee.setTotalWorkingHours(totalHours);
+        employee.setTotalWage(totalWage);
     }
 
     @Override
